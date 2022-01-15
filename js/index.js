5a2a9ec18019f85a6b52682d522fea3b0ee9c596
alexandra koletsos <alexkoletsos@alexandras-MacBook-Pro.local>
2022-01-15 15:22:53 -0500

    jan 15

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	console.log("hello!");
+	var myFavoriteBook = "Where the Crawdads Sing";
+	console.log("My favorite book is " + myFavoriteBook);
+
+	//window.onclick = changeBG;
+
+	//function changeBG(){
+		//document.getElementsByTagName("Body")[0].style.backgroundColor = "#C1E3B0";
+
+	//}
+
+	let c = init("canvas"),
+  w = (canvas.width = window.innerWidth),
+  h = (canvas.height = window.innerHeight);
+//initiation
+
+
+
+
+});
